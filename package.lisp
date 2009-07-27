e28367637359897a54031ca98b9d508caf548ae9
Marcus Eskilsson <marcus@sxemacs.org>
2009-07-27 16:24:39 +0200

    shadowing-import

--- package.lisp
+++ package.lisp
@@ -1,5 +1,7 @@
 ;;; -*- mode: lisp; indent-tabs: nil -*-
 
 (defpackage :cluck-hunt
-  (:use :lispbuilder-sdl :cl)
+  (:use :lispbuilder-sdl :lispbuilder-sdl-image :cl)
+  (:shadowing-import-from :lispbuilder-sdl-image :load-image
+			  :image-p :image-type-of)
   (:export #:cluck-hunt))
